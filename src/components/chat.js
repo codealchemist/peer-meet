07faf67d3c515aa1b89c58886ecef2317768ca0c
Alberto Miranda <codealchemist@gmail.com>
2020-04-15 19:47:30 -0300

    Added p2p video

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,4 +1,5 @@
-import React, { useGlobal, useState } from 'reactn'
+import React, { useGlobal, useState, useEffect } from 'reactn'
+import styled from 'styled-components'
 import { nanoid } from 'nanoid'
 import Peer from 'services/peer'
 import Signaling from 'services/signaling'
@@ -8,6 +9,7 @@ const logger = new Logger('CHAT')
 const connections = {}
 const id = nanoid(12) // Our own id.
 const localId = id
+let localStream = null
 const initiatorId = getRemoteId()
 const signaling = new Signaling(id, initiatorId)
 signaling.init(id)
@@ -21,7 +23,7 @@ if (initiatorId) {
 }
 
 const messageActionsMap = {
-  request: ({ remoteId, peer, signaling }) => {
+  request: ({ remoteId, peer, signaling, isInitiator }) => {
     logger.log(`Got request from ${remoteId}:`, { peer, signaling })
     logger.log(
       `Got request message from ${remoteId}. We will send signals to this peer when available.`
@@ -38,7 +40,7 @@ const messageActionsMap = {
   candidate: ({ peer, signal, signaling }) => {
     logger.log('Got candidate', { peer, signal, signaling })
     peer.connect(signal)
-  },
+  }
 }
 
 // Local signals.
@@ -55,21 +57,34 @@ const signalActionMap = {
   candidate: ({ id, remoteId, signal, peer, signaling }) => {
     logger.log('Got CANDIDATE signal, sending it...')
     signaling.send({ type: 'candidate', targetId: remoteId, signal })
-  },
+  }
 }
 
-function createPeer(remoteId, type) {
+function createPeer({ remoteId, type, addStream, removeStream }) {
   const isInitiator = type === 'request'
 
   const peer = new Peer({
     id,
     isInitiator,
+    stream: localStream
   })
   peer
     .onSignal(({ signal, id }) => {
       // Signals received from the Peer object are always local.
       const type = signal.type || 'candidate'
       signalActionMap[type]({ id, remoteId, signal, peer, signaling })
+    })
+    .onStream(({ stream, id }) => {
+      logger.log(`Set STREAM from ${id} 🎬`)
+      addStream({ id: remoteId, peer, stream })
+    })
+    .onClose(() => {
+      logger.log(`CLOSED ${remoteId} `)
+      removeStream(remoteId)
+    })
+    .onError(({ id, error }) => {
+      logger.log(`ERROR on ${remoteId} ❌`, error)
+      removeStream(remoteId)
     })
     .init()
 
@@ -90,6 +105,8 @@ function getPeer({
   type,
   totalConnections,
   setTotalConnections,
+  addStream,
+  removeStream
 }) {
   if (connections[remoteId]) {
     logger.log(`FOUND CONNECTION for ${remoteId}:`, connections[remoteId])
@@ -97,7 +114,7 @@ function getPeer({
   }
 
   // Create a new peer.
-  const peer = createPeer(remoteId, type)
+  const peer = createPeer({ remoteId, type, addStream, removeStream })
   logger.log('NEW PEER created', peer)
 
   connections[remoteId] = {
@@ -110,8 +127,62 @@ function getPeer({
   return peer
 }
 
+const RemoteStreamsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  video {
+    width: 40vw;
+  }
+`
+
+const LocalVideo = styled.video`
+  width: 100px;
+  position: fixed;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+`
+
+const Title = styled.h2`
+  text-align: left;
+  margin: 10px;
+  cursor: pointer;
+`
+
+const Stats = styled.div`
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+`
+
 function Chat() {
   const [totalConnections, setTotalConnections] = useState(0)
+  const [showLocalVideo, setShowLocalVideo] = useState(true)
+  const streamsContainerId = 'streams-container'
+  let streamsContainerEl = null
+  const localVideoEl = React.createRef()
+
+  const addStream = ({id, peer, stream}) => {
+    logger.log('GOT REMOTE STREAM 🎬', peer)
+    const video = document.createElement('video')
+    video.srcObject = stream
+    video.id = id
+    if (!streamsContainerEl) {
+      logger.log('EMPTY CONTAINER!', streamsContainerEl)
+      return
+    }
+    streamsContainerEl.appendChild(video)
+    video.play()
+  }
+
+  const removeStream = (id) => {
+    const video = document.getElementById(id)
+    if (!video) return
+    video.remove()
+  }
 
   signaling.onRemoteSignal(({ id, targetId, type, signal }) => {
     if (targetId && targetId !== localId) {
@@ -131,27 +202,58 @@ function Chat() {
       type,
       totalConnections,
       setTotalConnections,
+      addStream,
+      removeStream
     })
     messageActionsMap[type]({
       remoteId: id,
       peer,
       signal,
-      signaling,
+      signaling
     })
+  })
+
+  useEffect(() => {
+    streamsContainerEl = document.querySelector(`#${streamsContainerId}`)
+
+    // Get browser media stream.
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then((stream) => {
+      logger.log('---- GOT LOCAL STREAM 🎬', stream)
+      localStream = stream
+      localVideoEl.current.srcObject = localStream
+    }).catch(() => {})
   })
 
   return (
     <>
-      <h2>{localId}</h2>
-      🔌 {totalConnections}
+      <Title onClick={() => setShowLocalVideo(true)}>{localId}</Title>
+      <Stats>
+        🔌 {totalConnections}
+      </Stats>
+
+      { showLocalVideo &&
+        <LocalVideo
+          ref={localVideoEl}
+          onClick={() => setShowLocalVideo(false)}
+          autoPlay
+        /> 
+      }
+      <RemoteStreamsContainer id="streams-container"></RemoteStreamsContainer>
     </>
   )
 }
 
-window.test = () => {
+window.test = (msg) => {
   Object.entries(connections).map(([id, connection]) => {
     logger.log(`== SENDING TEST MESSAGE from ${localId} to ${id}`)
-    connection.peer.send({ m: `Hello ${id}! This is ${localId} :)` })
+    connection.peer.send({
+      from: localId,
+      to: id,
+      m: msg || `Hello ${id}! This is ${localId} :)`
+    })
   })
 }
 export default Chat
